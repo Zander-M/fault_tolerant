97d57fe0e23f13848a422075f98f9a6d4386b5b0
Zander-M <zm800@nyu.edu>
2021-04-08 12:15:35 +0800

    modifying ant model, simulate failure,

--- test/env_tester.py
+++ test/env_tester.py
@@ -1,9 +1,15 @@
 import gym
-import mujoco_py
+import numpy as np
 
 e = gym.make("gym_fault:fault-v0")
+print(e.model.geom_size)
+actionSpaceSize = len(e.action_space.sample())
+noAction = np.zeros(actionSpaceSize)
 while (1):
-    action = e.action_space.sample()
+    # action = e.action_space.sample()
+    action = noAction # put actions here
+    e.model.geom_size[4][1] += 1e-4 # change model on the fly! 
+    e.model.geom_rgba[4] = np.array([1., 0., 0., 1.], dtype='float32') # change fault color
     observation, reward, done, info = e.step(action)
     e.render()
     if done:
