c6f3fb8fd72ad9fb6a6a5fee5e5d5ee62fb43a38
Zander-M <zm800@nyu.edu>
2021-04-14 14:48:09 +0800

    update env & create pytorch test

---
+++ src/train/predStruct/trasitionDataset.py
@@ -0,0 +1,18 @@
+'''
+    Trasition Dataset. This dataset contains the state transitions, state observations
+    and their corresponding hardware representation.
+'''
+from torch.utils.data import Dataset
+
+DATA_PATH = ""
+
+class TransitionDataset(Dataset):
+
+    def __init__(self, error):
+        self.error
+        pass
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
